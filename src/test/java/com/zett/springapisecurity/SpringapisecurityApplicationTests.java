e8c8ab8c54752eca33dceb5e676eb37359201210
NguyenQuang <quangnn906@gmail.com>
2024-10-15 18:47:21 +0700

    Step 1: Create Base Project

---
+++ src/test/java/com/zett/springapisecurity/SpringapisecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zett.springapisecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringapisecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
